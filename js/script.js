7fa7718922ad2ae2519ad0fedd805220ee8d5422
kimannaseulgi <kimanna@umich.edu>
2024-12-06 20:21:41 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
-// expand card
 function toggleCard(card) {
-
     const cardDetails = card.querySelector(".card-details");
 
-    if (cardDetails.style.display == "block") {
+    // prefers-reduced-motion
+    const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+
+    if (prefersReducedMotion) {
+        // skip any animations
+        cardDetails.style.transition = "none";
+    }
+
+    // toggle visibility
+    if (cardDetails.style.display === "block") {
         cardDetails.style.display = "none";
     } else {
         cardDetails.style.display = "block";
     }
 
     console.log("card expanded: ", card);
-};
+}
 
-// keyboard access
 function handleKeyPress(event, element) {
     if (event.key === "Enter") {
         event.preventDefault();
         toggleCard(element);
     }
-};
+}
 
 // footer year
 document.getElementById("year").innerHTML = new Date().getFullYear();
